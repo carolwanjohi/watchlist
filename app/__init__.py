34b52527d5dedab2111f322fa1361a50d0a8aa37
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-10-15 13:00:35 +0300

    API Calls and Macros Set up request for API Calls Fix SSL issue Create macros to diaply movies list

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from .config import DevConfig
 ''' 
     Pass instance_relative_config to connect to instace/
 '''
-app = Flask(__name__, instance_relative_config = True)
+app = Flask(__name__,instance_relative_config = True)
 
 # Setting upconfiguration
 app.config.from_object(DevConfig)
@@ -14,6 +14,6 @@ app.config.from_object(DevConfig)
     Connect to config.py file
     All its contents are appended to app.config
 '''
-app.config.from_pyfile('config.py')
+app.config.from_pyfile("config.py")
 
 from app import views
--- app/config.py
+++ app/config.py
@@ -2,23 +2,26 @@ class Config:
     '''
     General configuration parent class
     '''
-    # Store movie base URL
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+
+
 
 class ProdConfig(Config):
     '''
-    Pruduction configuration child class
-
-    Args:
-        Config: parent class with general config settings
-    '''
-    pass
-
-class DevConfig(Config):
-    '''
-    Development config child class
+    Pruduction  configuration child class
 
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
     DEBUG = True
