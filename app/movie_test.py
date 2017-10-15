bd0944dc09cefd8dc40eb454ef7f9e08aa1b9bff
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-10-15 18:16:19 +0300

    Add navbar, bootsrap css, custom css and show details of a single movie. Change attribute name in Movie object

--- app/movie_test.py
+++ app/movie_test.py
@@ -12,23 +12,10 @@ class MovieTest(unittest.TestCase):
         Set up method that will run before every Test
         '''
         
-        elf.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_movie,Movie))
-
-
-    # def test_init(self):
-    #     '''
-    #     Test initalisation of Movie class
-    #     '''
-
-    #     self.assertEqual( self.new_movie.id, 1234)
-    #     self.assertEqual( self.new_movie.title, 'Python Must Be Crazy' )
-    #     self.assertEqual( self.new_movie.overview, 'A thrilling new Python Series' )
-    #     self.assertEqual( self.new_movie.image, 'https://image.tmdb.org/t/p/w500/khsjha27hbs' )
-    #     self.assertEqual( self.new_movie.vote_average, 8.5 )
-    #     self.assertEqual( self.new_movie.vote_count, 129993 )
 
 if __name__ == '__main__':
     unittest.main()
--- app/requests.py
+++ app/requests.py
@@ -31,12 +31,46 @@ def get_movies(category):
 
     return movie_results
 
+def get_movie(id):
+
+    '''
+    Function to get a movie
+
+    Args:
+        id : movie id
+
+    Returns:
+        movie_object : A movie object
+    '''
+        
+    get_movie_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
+
+        movie_object = None
+        if movie_details_response:
+            id = movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+            vote_average = movie_details_response.get('vote_average')
+            vote_count = movie_details_response.get('vote_count')
+
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+
+    return movie_object
+
+
 
 def process_results(movie_list):
     '''
     Function  that processes the movie result and transform them to a list of Objects
+    
     Args:
         movie_list: A list of dictionaries that contain movie details
+    
     Returns :
         movie_results: A list of movie objects
     '''
@@ -49,11 +83,10 @@ def process_results(movie_list):
         vote_average = movie_item.get('vote_average')
         vote_count = movie_item.get('vote_count')
 
-        # if poster:
-        #     movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
-        #     movie_results.append(movie_object)
-            
         movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
         movie_results.append(movie_object)
 
     return movie_results
+
+
+
