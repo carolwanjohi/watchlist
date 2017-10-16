f7d68c028a59b95bb7bff539eaebff0c11acb629
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-10-16 16:27:12 +0300

    Create test for initialise of movie object

--- app/movie_test.py
+++ app/movie_test.py
@@ -12,10 +12,18 @@ class MovieTest(unittest.TestCase):
         Set up method that will run before every Test
         '''
         
-        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','khsjha27hbs',8.5,129993)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_movie,Movie))
 
+    def test_init(self):
+        self.assertEqual(self.new_movie.id, 1234)
+        self.assertEqual(self.new_movie.title,'Python Must Be Crazy')
+        self.assertEqual(self.new_movie.overview,'A thrilling new Python Series')
+        self.assertEqual(self.new_movie.poster,'https://image.tmdb.org/t/p/w500/khsjha27hbs')
+        self.assertEqual(self.new_movie.vote_average,8.5)
+        self.assertEqual(self.new_movie.vote_count,129993)
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
