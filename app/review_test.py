46261f75458be711cccc22989542a5cdf66e2a6a
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-10-16 19:05:53 +0300

    Create functionality for writing reviews on a movie and displaying the reviews. Using wtforms

---
+++ app/review_test.py
@@ -0,0 +1,66 @@
+import unittest
+from models import review
+
+Review = review.Review
+
+class ReviewTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Review class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        
+        self.new_review = Review(12345,'Review for movies',"https://image.tmdb.org/t/p/w500/jdjdjdjn",'This movie is the best thing since sliced bread')
+
+    def tearDown(self):
+        '''
+        Using clear_reviews method to delete reviews from all_reviews list
+        '''
+        Review.clear_reviews()
+
+    def test_instance(self):
+        self.assertTrue( isinstance( self.new_review, Review) )
+
+    def test_init(self):
+        '''
+        Test initialisation of Review class
+        '''
+        self.assertEqual( self.new_review.movie_id, 12345)
+        self.assertEqual( self.new_review.title, 'Review for movies')
+        self.assertEqual( self.new_review.imageurl, "https://image.tmdb.org/t/p/w500/jdjdjdjn")
+        self.assertEqual( self.new_review.review, 'This movie is the best thing since sliced bread')
+
+    def test_save_review(self):
+        '''
+        Test if reveiws are saving
+        '''
+        self.new_review.save_review()
+
+        # self.assertEqual( len(Review.all_reviews) , 1)
+        self.assertTrue( len(Review.all_reviews) > 0)
+
+    def test_get_reviews(self):
+        '''
+        Test if reviews for a specific movie gotten by the id
+        '''
+        self.new_review.save_review()
+
+        test_review = Review(67890, 'Review for Why', "https://image.tmdb.org/t/p/w500/ghighighi", 'The aswesome Why on a journey')
+        test_review.save_review()
+
+        gotten_reviews = Review.get_reviews(67890)
+
+        # self.assertEqual( len(gotten_reviews), 1)
+        self.assertTrue( len(gotten_reviews) == 1)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .requests import get_movies,get_movie,search_movie
+from .models import review
+from .forms import ReviewForm
+
+Review = review.Review
 
 # Views
 @app.route('/')
@@ -32,8 +36,9 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
     
-    return render_template('movie.html',title = title, movie = movie)
+    return render_template('movie.html',title = title, movie = movie, reviews = reviews)
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -46,5 +51,27 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'Search results for {movie_name}'
     return render_template('search.html', movies = searched_movies)
+
+@app.route('/movie/review/new/<int:id>', methods=['GET','POST'])
+def new_review(id):
+
+    '''
+    View function to display form for writting a movie review
+    '''
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id, title, movie.poster, review)
+        new_review.save_review()
+        return redirect(url_for('movie', id = movie.id))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html', title = title, review_form = form, movie = movie)
+
+
+
 
 
